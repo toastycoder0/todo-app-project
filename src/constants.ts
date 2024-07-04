1bf2352eb0ee9f43c0248edb2c17d0d2c452452e
toastycoder0 <silentworker36743@joseluria.com>
2024-07-03 23:13:56 -0500

    feat: added view constants

--- src/constants.ts
+++ src/constants.ts
@@ -4,3 +4,11 @@ export const THEME_VALUES = {
 }
 
 export const DARK_MEDIA_QUERY = '(prefers-color-scheme: dark)'
+
+export const VIEWS = {
+  all: { name: 'All', key: 'all', accept: [true, false] },
+  active: { name: 'Active', key: 'active', accept: [false] },
+  completed: { name: 'Completed', key: 'completed', accept: [true] }
+}
+
+export const VIEWS_LIST = Object.values(VIEWS)
