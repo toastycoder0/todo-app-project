99eb458aa6b7e730c952912891e809afb401e507
toastycoder0 <silentworker36743@joseluria.com>
2024-07-03 23:16:43 -0500

    feat: implemented todo app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,35 @@
+import { useState } from 'react'
+import { ThemeProvider } from './components/ThemeProvider'
+import { Header } from './components/Header'
+import { ItemsList } from './components/ItemsList'
+import { ItemElement } from './components/ItemElement'
+import { getItemsStore } from './itemsStore'
+import { VIEWS, VIEWS_LIST } from './constants'
+
 const App = () => {
+  const [view, setView] = useState(VIEWS.all.key)
+  const { items, onAddItem, onCheckItem, onDeleteItem, onClearCompleteItems } = getItemsStore()
+  const itemsToRender = items.filter(({ checked }) => {
+    const { accept } = VIEWS_LIST.find(({ key }) => key === view) ?? VIEWS.all
+    return accept.includes(checked)
+  })
+
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ThemeProvider>
+      <div className='w-full max-w-[33.75rem]'>
+        <Header onNewItem={(item) => onAddItem(item)} />
+        <ItemsList
+          uncompleted={items.filter(({ checked }) => checked === false).length}
+          selectedView={view}
+          onSelectView={(selectedView) => setView(selectedView)}
+          onClearItems={onClearCompleteItems}
+        >
+          {itemsToRender.map((item) => (
+            <ItemElement {...item} onDelete={onDeleteItem} onCheck={onCheckItem} />
+          ))}
+        </ItemsList>
+      </div>
+    </ThemeProvider>
   )
 }
 
